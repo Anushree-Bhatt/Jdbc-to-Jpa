46424abfc40705e1d573e60202de7d48dcc610f6
Anushree <anushreebhatt357@gmail.com>
2025-07-04 17:23:40 -0400

    added multiple rows to table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO Person (id, name, location, birth_date)
-VALUES (
-   1000, 'John', 'San Fransisco, USA', current_date()
-);
+VALUES
+    (1000, 'John', 'San Fransisco, USA', current_date()),
+    (1001, 'Peter', 'Long beach, USA', current_date()),
+    (1002, 'Andrew', 'Florida, USA', current_date());

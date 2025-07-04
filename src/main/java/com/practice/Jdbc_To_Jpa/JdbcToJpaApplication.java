a256a607f91e77029fc55e4d6881bc992f5cc62f
Anushree <anushreebhatt357@gmail.com>
2025-07-04 16:49:03 -0400

    Setup

---
+++ src/main/java/com/practice/Jdbc_To_Jpa/JdbcToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.Jdbc_To_Jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcToJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/Jdbc_To_Jpa/JdbcToJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.Jdbc_To_Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcToJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
